16c21d3ab06adc7eff9cac94efff38fb823de408
Dor Azaria <46644036+DorAzaria@users.noreply.github.com>
2021-03-20 20:38:28 +0200

    Floyd Warshall Weights

--- FloydWarshall/Java/FW_WeightsOnEdgesAndVertices.java
+++ FloydWarshall/Java/FW_WeightsOnEdgesAndVertices.java
@@ -1,61 +1,69 @@
 package FloydWarshall.Java;
 
-public class FW_WeightsOnVertices {
+public class FW_WeightsOnEdgesAndVertices {
     static final int INF = 9999999;
 
     public static void main(String[] args) {
-        int[] values = {1,7,10,5};
+        int[] values = {1, 7, 10, 5};
         int[][] matrix = {
-                {0,1,INF,1},
-                {1,0,1,INF},
-                {INF,1,0,1},
-                {1,INF,1,0}
+                {0, 5, INF, 20},
+                {5, 0, 10, INF},
+                {INF, 10, 0, 15},
+                {20, INF, 15, 0}
         };
-
         System.out.println("GIVEN ORIGINAL MATRIX: (* = infinity): ");
         print(matrix);
-        FloydWarshall(matrix,values);
-    }// END main()
+        int[][] solution = FloydWarshall(matrix,values);
+    }
+    public static int[][] FloydWarshall(int[][] matrix, int[] values) {
+        int[][] temp = new int[matrix.length][matrix.length];
 
-    public static void FloydWarshall(int[][] matrix, int[] values) {
 
-        System.out.println("SET WEIGHTS ON EDGE NEIGHBORS BY (Vi + Vj): ");
+        System.out.println("SET AND COPY TO NEW MATRIX THE FOLLOWING FORMULA: ");
+        System.out.println("( TEMP[I][J] = 2*MATRIX[I][J] + VALUES[I] + VALUES[J] ):");
         for(int i = 0; i < matrix.length; i++) {
             for(int j = 0; j < matrix.length; j++) {
-                if(matrix[i][j] == 0) {
-                    matrix[i][j] = values[i];
+                if(i==j) {
+                    temp[i][j] = values[i];
                 }
-                else if(matrix[i][j] == 1) {
-                    matrix[i][j] = values[i] + values[j];
+                else if(matrix[i][j] > 0 && matrix[i][j] != INF) {
+                    temp[i][j] = 2*matrix[i][j] + values[i] + values[j];
+                }
+                if(matrix[i][j] == INF) {
+                    temp[i][j] = INF;
                 }
             }
         }
-        print(matrix);
+        print(temp);
 
         System.out.println("FOLLOWING FLOYD WARSHALL ALGO (WITH WRONG WEIGHTS): ");
         for(int k = 0 ; k < matrix.length; k++) {
             for(int i = 0 ; i < matrix.length; i++) {
                 for(int j = 0 ; j < matrix.length; j++) {
                     if(i != j) {
-                        matrix[i][j] = Math.min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+                        temp[i][j] = Math.min(temp[i][j], temp[i][k] + temp[k][j]);
                     }
                 }
             }
         }
-        print(matrix);
+        print(temp);
 
-        System.out.println("FIXING WRONG WEIGHTS BY (V[SRC] + MATRIX[SRC][DST] + V[DST])/2 ");
+        System.out.println("FIXING WRONG WEIGHTS BY (V[SRC] + TEMP[SRC][DST] + V[DST])/2 ");
+        int[][] solution = new int[matrix.length][matrix.length];
         for(int i = 0 ; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
                 if(i != j) {
-                    matrix[i][j] = (values[i] + matrix[i][j] + values[j])/2;
+                    solution[i][j] = (values[i] + temp[i][j] + values[j])/2;
+                } else {
+                    solution[i][j] = values[i];
                 }
             }
         }
-        print(matrix);
+        print(solution);
+        return solution;
     } // END OF FloydWarshall() METHOD
 
-/************************************************************************************/
+    /************************************************************************************/
     public static void print(int[][] matrix) {
         for(int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
