b126bf9f95216ba4f405fc45f97d88c7b9b5f056
Dor Azaria <46644036+DorAzaria@users.noreply.github.com>
2021-05-04 21:03:34 +0300

    Dijkstra Matrix Implementation

--- Dijkstra/MatrixImplementation/Test.java
+++ Dijkstra/MatrixImplementation/Test.java
@@ -8,7 +8,7 @@ public class Test {
     public static void main(String[] args) {
         DijkstraMatrix dijkstraMatrix = new DijkstraMatrix();
 
-        int[][] mat = {{0 ,1 ,2 ,inf ,inf ,inf ,inf ,inf },
+        int[][] matrix = {{0 ,1 ,2 ,inf ,inf ,inf ,inf ,inf },
                         {1 ,0 ,4 ,inf ,5 ,inf ,inf ,inf },
                         {2 ,4 ,0 ,7 ,inf ,3 ,inf ,inf },
                         {inf ,inf ,7 ,0 ,3 ,8 ,inf ,inf },
@@ -16,9 +16,9 @@ public class Test {
                         {inf ,inf ,3 ,8 ,inf ,0 ,2 ,inf },
                         {inf ,inf ,inf ,inf ,1 ,2 ,0 ,5 },
                         {inf ,inf ,inf ,inf ,4 ,inf ,5 ,0 }};
-        dijkstraMatrix.Dijkstra(initGraph(mat),0,7,mat);
+        dijkstraMatrix.Dijkstra(initGraph(matrix),0,7,matrix); // 10
 
-        mat = new int[][] {{0,2,inf,inf,inf,inf,inf},
+        matrix = new int[][] {{0,2,inf,inf,inf,inf,inf},
                            {2, 0, 2, 5, inf,inf,inf},
                            {inf,2,0,inf,inf,inf,inf},
                            {inf, 5,inf, 0, 7, 5,inf},
@@ -26,22 +26,17 @@ public class Test {
                            {inf,inf,inf,5,inf,0,inf},
                            {inf,inf,inf,inf,2,inf,0}
                         };
-        dijkstraMatrix.Dijkstra(initGraph(mat),0,6,mat);
+        dijkstraMatrix.Dijkstra(initGraph(matrix),0,6,matrix); // 16
     }
 
     public static ArrayList<ArrayList<Integer>> initGraph(int[][] matrix) {
         ArrayList<ArrayList<Integer>> G = new ArrayList<>();
-
-        for (int i = 0; i < matrix.length+1; i++) {
-            G.add(new ArrayList<>());
-        }
-
         for(int i = 0; i < matrix.length; i++) {
             for(int j = 0; j < matrix.length; j++) {
+                if(j == 0) G.add(new ArrayList<>());
                 if(matrix[i][j] != inf)  G.get(i).add(j);
             }
         }
-
         return G;
     }
 }
