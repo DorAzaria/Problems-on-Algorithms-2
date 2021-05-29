7c173f405c50913b15863d227ff77c4ef3322fcc
Dor Azaria <46644036+DorAzaria@users.noreply.github.com>
2021-05-29 17:12:44 +0300

    Generate tree by degree series

--- BuildTreeFromDegrees/BuildTreeFromDegreesAlgorithm.java
+++ BuildTreeFromDegrees/BuildTreeFromDegreesAlgorithm.java
@@ -1,49 +1,39 @@
 package BuildTreeFromDegrees;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BuildTreeFromDegreesAlgorithm {
 
-    public ArrayList<Integer>[] initTreeFromDegrees(int[] degrees){
-
+    public int[] getTree(int[] degrees) {
         int sum = 0;
-        for(int i = 0; i < degrees.length; i++) { // CHECK if it is a tree
-            sum += degrees[i];
+
+        for(int degree : degrees) {
+            sum += degree;
         }
 
-        if(sum % 2 != 0) {
-            System.out.println("THIS GRAPH IS NOT A TREE!");
-            return new ArrayList[degrees.length];
+        if((sum / 2) + 1 != degrees.length) {
+            System.out.println("This degree array can't generate a tree due to definition");
+            return new int[] {};
         }
 
-        Arrays.sort(degrees); // O(N)
-        reverseArray(degrees); // O(N)
-        int n = degrees.length;
-        ArrayList[] tree = new ArrayList[n];
-
-        for (int i = 0; i < tree.length; i++) { // O(N)
-            tree[i] = new ArrayList<Integer>();
-        }
-
-        int vertex = 1;
-        for (int i = 0; i < tree.length; i++) { // O(N)
-            for (int j=0; j<degrees[i]; j++) { // O(|Adj(i)|)
-                tree[i].add(vertex);
-                tree[vertex].add(i);
-                degrees[vertex]--;
-                vertex++;
+        int[] tree = new int[degrees.length];
+        Arrays.sort(degrees);
+        int j = 0;
+        for(int i = 0; i < degrees.length; i++) {
+            if(degrees[i] > 1){
+                j = i;
+                break;
             }
-            degrees[i] = 0;
         }
 
-        return tree;
-    }
-    private void reverseArray(int[] arr){
-        for (int i=0; i<arr.length/2; i++) {
-            int temp = arr[i];
-            arr[i] = arr[arr.length-i-1];
-            arr[arr.length-i-1] = temp;
+        for(int i = 0; i < degrees.length-2; i++) {
+            tree[i] = j;
+            degrees[j]--;
+
+            if(degrees[j] == 1) {
+                j++;
+            }
         }
+        tree[degrees.length-1] = degrees.length;
+        return tree;
     }
 }
--- BuildTreeFromDegrees/Test.java
+++ BuildTreeFromDegrees/Test.java
@@ -1,6 +1,6 @@
 package BuildTreeFromDegrees;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Test {
 
@@ -10,19 +10,19 @@ public class Test {
         int[] degree = {1,1,1,1,2,2,3,3};
         BuildTreeFromDegreesAlgorithm algo = new BuildTreeFromDegreesAlgorithm();
         System.out.println("/////////// TEST 1");
-        printGraph(algo.initTreeFromDegrees(degree));
+        System.out.println(Arrays.toString(algo.getTree(degree)));
 
         //////////////////////////////////////// TEST 2
         degree = new int[] {4,3,2,1,1,1,1,1};
         algo = new BuildTreeFromDegreesAlgorithm();
         System.out.println("/////////// TEST 2");
-        printGraph(algo.initTreeFromDegrees(degree));
+        System.out.println(Arrays.toString(algo.getTree(degree)));
 
         //////////////////////////////////////// TEST 3
         degree = new int[] {1,1,3,3,1,4,1,1,1};
         algo = new BuildTreeFromDegreesAlgorithm();
         System.out.println("/////////// TEST 3");
-        printGraph(algo.initTreeFromDegrees(degree));
+        System.out.println(Arrays.toString(algo.getTree(degree)));
 
         //////////////////////////////////////// TEST 4
         /*                  (6)
@@ -35,24 +35,7 @@ public class Test {
         degree = new int[] {6,0,0,0,0,0,0,0};
         algo = new BuildTreeFromDegreesAlgorithm();
         System.out.println("/////////// TEST 4");
-        printGraph(algo.initTreeFromDegrees(degree));
+        System.out.println(Arrays.toString(algo.getTree(degree)));
 
-    }
-
-    public static void printGraph(ArrayList<Integer>[] graph) {
-        if(graph.length != 0) {
-            int i = graph.length - 1;
-            for (ArrayList<Integer> list : graph) {
-                if (list != null && !list.isEmpty()) {
-                    System.out.print("Node (" + i + ") neighbours are : [");
-                    for (Integer j : list) {
-                        System.out.print(j + ",");
-                    }
-                    System.out.println("]");
-                    i--;
-                }
-            }
-            System.out.println();
-        }
     }
 }
