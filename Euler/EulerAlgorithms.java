3cd10cd931272b1ec5e28e74e7d87f94e854b897
Dor Azaria <46644036+DorAzaria@users.noreply.github.com>
2021-06-14 20:17:20 +0300

    Eulerian Graphs

--- Euler/EulerAlgorithms.java
+++ Euler/EulerAlgorithms.java
@@ -5,7 +5,45 @@ import java.util.Stack;
 
 public class EulerAlgorithms {
 
+
     public Stack<Integer> EulerianCycle(ArrayList<ArrayList<Integer>> graph, int start_node) {
+        if(hasEulerianCycle(graph)) // there is a cycle
+            return EulerianAlgorithm(graph,start_node);
+        return new Stack<>();
+    }
+
+    private boolean hasEulerianCycle(ArrayList<ArrayList<Integer>> graph) {
+        for(ArrayList<Integer> list : graph) {
+            if (list.size() % 2 == 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Stack<Integer> EulerianPath(ArrayList<ArrayList<Integer>> graph) {
+        int start_index = hasEulerPath(graph);
+        return EulerianAlgorithm(graph,start_index);
+    }
+
+    private int hasEulerPath(ArrayList<ArrayList<Integer>> graph) {
+        int counter = 0;
+        int odd_index = -1;
+
+        for(int i = 0 ; i < graph.size(); i++) {
+            if(graph.get(i).size() % 2 == 1){
+                counter++;
+                odd_index = i;
+            }
+        }
+        if(counter == 2) { // then there is an euler path!
+            // return the start index of the path!
+            return odd_index;
+        }
+        return -1;
+    }
+
+    private Stack<Integer> EulerianAlgorithm(ArrayList<ArrayList<Integer>> graph, int start_node) {
         if(start_node >= graph.size() || start_node < 0) {
             return new Stack<>();
         }
@@ -30,27 +68,6 @@ public class EulerAlgorithms {
         return path;
     }
 
-    private int hasEulerPath(ArrayList<ArrayList<Integer>> graph) {
-        int counter = 0;
-        int odd_index = -1;
 
-        for(int i = 0 ; i < graph.size(); i++) {
-            if(graph.get(i).size() % 2 == 1){
-                counter++;
-                odd_index = i;
-            }
-        }
-
-        if(counter == 2) { // then there is an euler path!
-            // return the start index of the path!
-            return odd_index;
-        }
-        return -1;
-    }
-
-    public Stack<Integer> EulerianPath(ArrayList<ArrayList<Integer>> graph) {
-        int start_index = hasEulerPath(graph);
-        return EulerianCycle(graph,start_index);
-    }
 
 }
